7efe202a4d226178794fc9c52a602454fe8b7d6e
yahia mohamed nabil <99214644+Yahia0mohamed@users.noreply.github.com>
2024-08-15 18:42:47 +0300

    resetting project

--- DDRF/socialmedia/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'Fname', 'Lname')
-    search_fields = ('email', 'Fname', 'Lname')
--- DDRF/socialmedia/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('profile/', views.UserProfileView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user_profile'),
-    path('friends/', views.FriendListView.as_view({'get': 'list'}), name='friend_list'),
-    path('friend-requests/', views.FriendRequestView.as_view({'post': 'create'}), name='friend_request'),
-    path('users/', views.UserProfileView.as_view({'post': 'create'}), name='create_user'),
-]
