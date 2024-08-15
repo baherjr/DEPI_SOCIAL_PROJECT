5c58ba732ec93e7e9358d8ddf68b2b9eee4c0b82
baherjr <omarbaher215@gmail.com>
2024-08-15 20:34:38 +0300

    Revert "cc"
    This reverts commit dbd4a74a5fed1c456193c56866f4de3d6f51d5e7.

--- linkee/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class UserProfile(AbstractUser):
-    pass
-
-class Group(models.Model):
-    name = models.CharField(max_length=255)
-    users = models.ManyToManyField(UserProfile, related_name='groups')
